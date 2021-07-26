547e144419524c050cac6d897c47f236107ec708
khayal-0508 <80122272+khayal-0508@users.noreply.github.com>
2021-07-26 21:56:37 +0400

    Update play.py

--- callsmusic/handlers/play.py
+++ callsmusic/handlers/play.py
@@ -22,7 +22,7 @@ async def play(_, message: Message):
     audio = (
         message.reply_to_message.audio or message.reply_to_message.voice
     ) if message.reply_to_message else None
-    response = await message.reply_text('<b>🔄 Processing...</b>', False)
+    response = await message.reply_text('<b>🔄 Hazırlanır...</b>', False)
     if audio:
         if round(audio.duration / 60) > DURATION_LIMIT:
             raise DurationLimitError(
@@ -74,7 +74,7 @@ async def play(_, message: Message):
             url = text_links[0].url
         else:
             await response.edit_text(
-                '<b>❌ You did not give me anything to play</b>',
+                '<b>❌ Mənə oxutmaq üçün heç nə vermədin</b>',
             )
             return
 
@@ -83,8 +83,8 @@ async def play(_, message: Message):
     if chat_id in callsmusic.active_chats:
         position = await queues.put(chat_id, file=file)
         await response.edit_text(
-            f'<b>#️⃣ Queued at position {position}</b>...',
+            f'<b>#️⃣ Musiqi növbəyə alındı. Sıra nömrəsi {position}</b>...',
         )
     else:
         await callsmusic.set_stream(chat_id, file)
-        await response.edit_text('<b>▶️ Playing...</b>')
+        await response.edit_text('<b>▶️ Oxuyur...</b>')
